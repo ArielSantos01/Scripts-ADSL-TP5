735e6bbc0b058475d7712a2ffe9dfce12c027b2a
ArielSantos01 <arielg.santos21@gmail.com>
2023-11-01 19:16:21 -0300

    ej1

--- ej1.sh
+++ ej1.sh
@@ -1,12 +1,80 @@
 #!/bin/bash
+# tp5_1
+# CPIO
+# Grupo 4 ASL
+#
+while :
+do
+  clear
+  tput cup 1 0
+  echo "Usuario: $LOGNAME"
+  tput cup 1 25
+  echo "Terminal: `tty`"
+  tput cup 1 65
+  echo "Fecha: `date +%d/%m/%y`"
+  tput cup 2 0
+  echo "UTN FRM                                                                         "
+  echo "ASL 2023                                                     Menu de Operaciones"
+  echo " "
+  echo "                      1) Buscar un archivo específico en cinta"
+  echo "                      2) Listar archivos de una cinta"
+  echo "                      3) Extraer archivos de un usuario"
+  echo "                      4) Salir"
+  echo " "
+  echo -n "                                    Ingrese su opcion: "
+  read opcion
+  case $opcion in
 
-# Buscar el último archivo .log que contiene la línea "menu.sh"
-latest_log=$(grep -l "menu.sh" /etc/log/logcopiadia/*.log | sort -r | head -1)
+1)
 
-if [ -z "$latest_log" ]; then
-    echo "No se encontraron archivos .log que contengan 'menu.sh'."
+  if [ $# -ne 1 ]
+  then
+    echo "Uso: $0 <nombre del archivo>"
     exit 1
-else
-    echo "El último archivo .log que contiene 'menu.sh' es: $latest_log"
-fi
+  fi
 
+  archivo="$1"
+  ultimo_log=ls -lt "/etc/log/logcopiadia/" | head -n 1
+  cinta="/dev/cinta"
+
+  if ultimo_log | grep archivo
+  then
+    less ultimo_log
+    echo "¿Desea restituir el archivo desde el dispositivo?"
+    read respuesta
+    case "$respuesta" in
+      S|s) 
+        if test -f "$archivo"
+        then
+          mv "$archivo" "archivo.bk$(date +%Y%m%d)"
+        fi
+          echo "PROCESANDO... ESPERE O CTRL+C PARA ABORTAR"
+          cpio -ic "$archivo" <("$cinta")
+          echo "SE RESTAURÓ EL ARCHIVO $archivo";;
+      *)
+        exit 0
+  else
+    echo "No se encontro el archivo en el último log."
+  fi;;
+2)
+  cpio -tc <("$cinta") | wc -l;;
+3)
+  if [ $# -ne 1 ]
+  then
+    echo "Uso: $0 <nombre de usuario>"
+    exit 1
+  fi
+
+  usuario="$1"
+  directorio_usuario="/tmp/copia$usuario"
+
+  if cat /etc/passwd | grep "$1"
+  then
+  cpio -t <("$cinta") | grep "$1" | cpio -idum <("$cinta") -D "$directorio_usuario"
+  else
+    echo "No se ha encontrado el usuario $usuario"
+    sleep 3
+    exit 1
+  fi;;
+*)
+  exit 0
