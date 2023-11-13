cc7914fa894407f7d6ec17bac59619cbb2d21e2b
Maunas <gonzamaunas@gmail.com>
2023-11-13 02:09:15 -0300

    Modificado tp4, Arreglado Ej 1-1 tp5

--- ej11.sh
+++ ej11.sh
@@ -7,30 +7,39 @@ if [ $# -ne 1 ]
   fi
 
   archivo="$1"
-  ultimo_log=$(ls -1t /etc/log/logcopiadia/*.log | head -1)
-  cinta="/dev/cinta/cinta.cpio"
+  carpeta_log="/etc/log/logcopiadia"
+  logs=$(ls -t "$carpeta_log")
+  ultimo_log=""
+for log in $logs; do
+	if grep -q "$archivo" "$carpeta_log/$log"; then
+		ultimo_log="$log"
+		break
+	fi
+done
 
-  if grep -q "$archivo" "$ultimo_log"
-  then
-    less $ultimo_log
-    echo "¿Desea restituir el archivo desde el dispositivo?"
+if [ "$ultimo_log" != "" ]; then
+    #Se modificó el script de backup para mostrar en la primera linea del log la direccion de la cinta"
+    cinta=$(head -n 1 "$carpeta_log/$ultimo_log")
+
+    echo "Archivo encontrado en el log: $ultimo_log"
+    echo "Dirección Cinta: $cinta"
+
+    echo "¿Desea restituir el archivo desde el dispositivo? [S/N]"
     read respuesta
     case "$respuesta" in
       S|s) 
         if test -f "$archivo"
         then
           mv "$archivo" "${archivo}.bkp$(date +%Y%m%d)"
+	fi
           echo "PROCESANDO... ESPERE O CTRL+C PARA ABORTAR"
           cpio -idm < "$cinta" "$archivo"
           echo "SE RESTAURÓ EL ARCHIVO $archivo"
-        else
-          echo "El archivo no existe"
-        fi
         ;;
       *)
         exit 0
         ;;
     esac
 else
-    echo "No se encontro el archivo en el último log."
+    echo "No se encontro el archivo en los últimos logs."
 fi
