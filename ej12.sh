7344b3d9783de6074b7f8baff38a1b895ec5fb65
Maunas <gonzamaunas@gmail.com>
2023-11-15 01:32:34 -0300

    Corregidos errores menores

--- ej12.sh
+++ ej12.sh
@@ -2,5 +2,9 @@
 echo "Contar cantidad de archivos de una cinta."
 echo "Ingresar la dirección del archivo de cinta"
 read cinta
+if ! test -f $cinta; then
+	echo "No es válido el archivo de cinta."
+	exit 1
+fi
 cantidad=`cpio -t < "$cinta" | wc -l`
 echo "La cantidad de archivos es $cantidad"
